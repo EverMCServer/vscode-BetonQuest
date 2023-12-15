88e6cf01928cc29cd20e4ed563d69194e7d58923
k-jiang <k@kjiang.net>
2023-12-15 13:52:53 +0800

    wip list editor, add / remove for TextAreaList

--- src/view/packageEditor/components/Sider/CommonList/Input/TextAreaList.tsx
+++ src/view/packageEditor/components/Sider/CommonList/Input/TextAreaList.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Button, Space } from "antd";
 import TextArea from "antd/es/input/TextArea";
+import { VscClose } from "react-icons/vsc";
+
 import { InputProps } from "./Common";
 
 export default function (props: InputProps) {
@@ -7,20 +10,48 @@ export default function (props: InputProps) {
     return (
         <>
             {valueArray.map((value, index) =>
-                <TextArea
+                <Space.Compact
+                    block
                     key={index}
-                    value={value}
-                    onChange={(e) => {
+                    style={index > 0 ? { marginTop: 4 } : undefined}
+                >
+                    <TextArea
+                        value={value}
+                        onChange={(e) => {
+                            let valueUpdate = props.value as string[] || [""];
+                            valueUpdate[index] = e.target.value;
+                            props.onChange(valueUpdate);
+                        }}
+                        placeholder={props.placeholder}
+                        autoSize={{ minRows: props.config?.minRows || 2, maxRows: props.config?.maxRows || 6 }}
+                        size="small"
+                    />
+                    {valueArray.length > 1 && <Button
+                        type="default"
+                        size="small"
+                        onClick={() => {
+                            let valueUpdate = props.value as string[] || [""];
+                            valueUpdate = [...valueUpdate.slice(0, index), ...valueUpdate.slice(index + 1)];
+                            props.onChange(valueUpdate);
+                        }}
+                    >
+                        <VscClose />
+                    </Button>}
+                </Space.Compact>
+            )}
+            <div style={{ marginTop: 4 }}>
+                <Button
+                    type="primary"
+                    size="small"
+                    onClick={() => {
                         let valueUpdate = props.value as string[] || [""];
-                        valueUpdate[index] = e.target.value;
+                        valueUpdate.push("");
                         props.onChange(valueUpdate);
                     }}
-                    placeholder={props.placeholder}
-                    autoSize={{ minRows: props.config?.minRows || 2, maxRows: props.config?.maxRows || 6 }}
-                    size="small"
-                    style={index > 0 ? { marginTop: 4 } : undefined}
-                />
-            )}
+                >
+                    Add
+                </Button>
+            </div>
         </>
     );
 }
