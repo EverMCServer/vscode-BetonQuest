05068ce3dae6947588a94836fbf815ed29f4cdc9
k-jiang <k@kjiang.net>
2024-01-05 19:36:53 +0800

    wip 1.0 Events editor

--- src/view/eventsEditor/index.tsx
+++ src/view/eventsEditor/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import App from './app';
+
 import './index.css';
 
-import App from './app';
+import '../style/vscodeButton.css';
+import '../style/vscodeCheckbox.css';
+import "../style/vscodeDivider.css";
+import "../style/vscodeInputNumber.css";
+import "../style/vscodePopover.css";
+import "../style/vscodeTooltip.css";
 
 const domNode = document.getElementById('root')!;
 const root = createRoot(domNode);
