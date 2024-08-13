9a1dd6d5dfdd097916d27f9c550d8fa7b7664dc1
k-jiang <k@kjiang.net>
2024-08-13 17:58:16 +0800

    add mandatory key prompt wip

--- server/src/ast/v2/Condition/ConditionArgumentMandatory.ts
+++ server/src/ast/v2/Condition/ConditionArgumentMandatory.ts
@@ -1,3 +1,5 @@
+import { CompletionItem, CompletionItemKind } from "vscode-languageserver";
+
 import { ArgumentsPatternMandatory } from "betonquest-utils/betonquest/Arguments";
 
 import { ConditionArgumentMandatoryType } from "../../node";
@@ -12,7 +14,7 @@ export class ConditionArgumentMandatory extends AbstractNodeV2<ConditionArgument
   readonly parent: ConditionArguments;
 
   private argumentStr: string;
-  private offsets: [offsetStart: number, stringStart: number, offsetEnd: number];
+  private offsets: [offsetStart: number, keyStart: number, keyEnd: number, valueStart: number, offsetEnd: number];
   private pattern: ArgumentsPatternMandatory;
 
   constructor(
@@ -27,18 +29,70 @@ export class ConditionArgumentMandatory extends AbstractNodeV2<ConditionArgument
     this.offsetEnd = offsets[2];
     this.parent = parent;
 
-    this.argumentStr = argumentStr;
-    this.offsets = offsets;
+    this.argumentStr = argumentStr;  // argumentStr has been trimed already.
     this.pattern = pattern;
 
     // Parse argumentStr
 
-    // Parse key
-    if (this.pattern.key) {
-      // TODO
+    // Split value
+    if (this.pattern && this.pattern.key) {
+      // Key-value pair
+      const strs = this.argumentStr.split(":");
+      if (strs.length > 0) {
+        // With ":"
+        // Calculate offsets
+        const str = strs.slice(1).join(":");
+        const pos2 = offsets[1] + strs[0].length;
+        this.offsets = [offsets[0], offsets[1], pos2, pos2 + 1, offsets[2]];
+        // Parse key
+        // TODO ...
+        // Parse value
+        this.addChild(new ConditionArgumentValue(str, [this.offsets[3], this.offsets[4]], this.pattern, this));
+      } else {
+        // Missing ":"
+        // Calculate offsets
+        this.offsets = [offsets[0], offsets[1], offsets[2], offsets[2], offsets[2]];
+        // Parse key
+        // TODO ...
+        // Warn about missing value
+        // TODO ...
+      }
+    } else {
+      // Value only
+      // Calculate offsets
+      this.offsets = [offsets[0], offsets[1], offsets[1], offsets[1], offsets[2]];
+      // Parse 
+      this.addChild(new ConditionArgumentValue(this.argumentStr, [this.offsets[3], this.offsets[4]], this.pattern, this));
+    }
+  }
+
+  // TODO: completion
+  getCompletions(offset: number, documentUri?: string): CompletionItem[] {
+    const completionItems: CompletionItem[] = [];
+
+    // Prompt key suggestions
+    if (this.offsets[0] < offset && offset <= this.offsets[1] || offset === this.offsets[2]) {
+      this.parent.kindConfig.argumentsPatterns
+        .mandatory.filter(o => !this.parent.argumentMandatoryStrs.some(s => o.key && s.trimStart().startsWith(o.key)))
+        .forEach(pattern => completionItems.push({
+          label: pattern.key!,
+          kind: CompletionItemKind.Snippet, // TODO: move it onto SemanticTokenType etc.
+          detail: pattern.name?.toString(),
+          documentation: pattern.tooltip,
+          command: {
+            title: "Prompt value suggestion",
+            command: "editor.action.triggerSuggest"
+          },
+          insertText: pattern.key + ":",
+        }));
     }
 
-    // Parse value
-    this.addChild(new ConditionArgumentValue(this.argumentStr, [this.offsets[1], this.offsets[2]], this.pattern, this));
+    // Prompt other value suggestions
+    if (this.offsets[0] < offset && offset <= this.offsets[1]) {
+      // TODO ...
+    }
+
+    completionItems.push(...super.getCompletions(offset, documentUri));
+    return completionItems;
   }
 }
