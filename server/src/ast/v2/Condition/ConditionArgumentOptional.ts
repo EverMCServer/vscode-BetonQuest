bc5241d506ff5713407d50bbea16141f0fb11229
k-jiang <k@kjiang.net>
2024-08-15 19:13:22 +0800

    parse optional arg key

--- server/src/ast/v2/Condition/ConditionArgumentOptional.ts
+++ server/src/ast/v2/Condition/ConditionArgumentOptional.ts
@@ -1,9 +1,11 @@
-import { CompletionItem, CompletionItemKind } from "vscode-languageserver";
+import { CompletionItem, CompletionItemKind, DiagnosticSeverity } from "vscode-languageserver";
 
 import { ArgumentsPatternOptional } from "betonquest-utils/betonquest/Arguments";
 
+import { DiagnosticCode } from "../../../utils/diagnostics";
 import { ConditionArgumentOptionalType } from "../../node";
 import { AbstractNodeV2 } from "../../v2";
+import { ConditionArgumentKey } from "./ConditionArgumentKey";
 import { ConditionArguments } from "./ConditionArguments";
 import { ConditionArgumentValue } from "./ConditionArgumentValue";
 
@@ -38,29 +40,45 @@ export class ConditionArgumentOptional extends AbstractNodeV2<ConditionArgumentO
     if (this.pattern && this.pattern?.format !== "boolean") {
       // Key-value pair
       const strs = this.argumentStr.split(":");
-      if (strs.length > 0) {
+      if (strs.length > 1) {
         // With ":"
         // Calculate offsets
         const str = strs.slice(1).join(":");
         const pos2 = offsets[1] + strs[0].length;
         this.offsets = [offsets[0], offsets[1], pos2, pos2 + 1, offsets[2]];
         // Parse key
-        // TODO ...
+        this.addChild(new ConditionArgumentKey(strs[0], [this.offsets[1], this.offsets[2]], this.pattern, this));
         // Parse value
         this.addChild(new ConditionArgumentValue(str, [this.offsets[3], this.offsets[4]], this.pattern, this));
       } else {
-        // Missing ":"
         // Calculate offsets
         this.offsets = [offsets[0], offsets[1], offsets[2], offsets[2], offsets[2]];
-        // Parse key
-        // TODO ...
+        if (strs.length > 0) {
+          // Missing ":"
+          // Parse key
+          this.addChild(new ConditionArgumentKey(strs[0], [this.offsets[1], this.offsets[2]], this.pattern, this));
+          // Warn about missing value
+          this.addDiagnostic(
+            [this.offsets[2], this.offsets[3]],
+            `Missing semicolon ":"`,
+            DiagnosticSeverity.Error,
+            DiagnosticCode.ArgumentKeyMissingSemicolon,
+            [{
+              title: `Add semicolon ":"`,
+              text: ":",
+            }]
+          );
+        } else {
+          // Diagnostic of missing the whole argument has been handled by the parent.
+          // We don't need to do anything here.
+        }
       }
     } else {
       // Boolean
       // Calculate offsets
       this.offsets = [offsets[0], offsets[1], offsets[2], offsets[2], offsets[2]];
       // Parse key
-      // TODO ...
+      this.addChild(new ConditionArgumentValue(this.argumentStr, [this.offsets[3], this.offsets[4]], this.pattern, this));
     }
   }
 
